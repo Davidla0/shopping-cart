ca1d5a3f315183c0acb4a4005f42d960195d9783
Davidla0 <93866871+Davidla0@users.noreply.github.com>
2022-12-18 10:22:19 +0200

    first setup frotend

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+
+import { App } from './pages/App';
+// import { ProductDetails } from './pages/ProductDetails';
+
+// Routes accesible from the main navigation (in AppHeader)
+const routes = [
+    {
+        path: '/',
+        component: <App />,
+    },
+    // {
+    //     path: '/details/:id',
+    //     component: <ProductDetails />,
+    // },
+
+
+
+]
+
+export default routes
